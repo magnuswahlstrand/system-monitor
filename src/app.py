ca19327e90a1a253eb044a8ab076755bc9ee6f4e
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-01-20 13:46:49 +0100

    Working example of changing states

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import random
 import yaml
 import json
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 INITIAL_INDEX = 0
 N_INDICES = 5
@@ -19,6 +19,15 @@ def state():
         current_state = "unknown"
     return json.dumps(current_state)
 
+
+@app.route('/')
+def home():
+    return redirect("/ui", code=302)
+
+
+@app.route('/ui')
+def ui():
+    return render_template('index.html')
 
 STATES = None
 if __name__ == '__main__':
