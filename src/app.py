ff1356d652a0c4fafddc5c76681163afa6654d74
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-01-20 12:20:50 +0100

    Generate string states

--- src/app.py
+++ src/app.py
@@ -4,15 +4,19 @@ from flask import Flask
 INITIAL_INDEX = 0
 N_INDICES = 5
 
+STATES = ["deploying",
+          "deploying_complete",
+          "testing",
+          "testing_complete",
+          "idle"]
+
 app = Flask(__name__)
 
 
 @app.route('/state')
 def state():
-    current_state = random.randint(0, N_INDICES - 1)
-
-    return str(current_state)
-
+    current_state = random.choice(STATES)
+    return current_state
 
 if __name__ == '__main__':
     app.run(debug=True)
